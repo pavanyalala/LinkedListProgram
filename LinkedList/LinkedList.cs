4adddfef09a5b35a482d3c556a30a5238783f4a1
pavanyalala <pavanyalala4508@gmail.com>
2021-08-05 09:21:36 +0530

    [pavan]:Elemets Added To Linked List

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -16,18 +16,54 @@ namespace LinkedList
             Node node = new Node(data);
             if (this.head == null)
             {
-                this.head = node;
+            this.head = node;
+        }
+            else
+            {
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+        Console.WriteLine("Inserted values : " + node.data);
+        }
+
+        /// <summary>
+        /// Uc-2
+        /// </summary>
+        /// <param name="data"></param>
+        public void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if(this.head == null)
+            {
+                this.head = newNode;
             }
             else
             {
-                Node temp = head;
-                while(temp.next != null)
-                {
-                    temp = temp.next;
-                }
-                temp.next = node;
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
             }
-            Console.WriteLine("Inserted values : " + node.data);
+        }
+        /// <summary>
+        /// Displays Linked List
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp = temp.next;
+            }
         }
     }
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -8,9 +8,10 @@ namespace LinkedList
         {
             Console.WriteLine("Hello World!");
             LinkedList linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
-            linkedList.Add(70);
+            linkedList.AddInReverseOrder(70);
+            linkedList.AddInReverseOrder(30);
+            linkedList.AddInReverseOrder(56);
+            linkedList.Display();
 
         }
     }
