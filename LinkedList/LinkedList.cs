d0182d18c4248fa473817dfbaa74515c195f2a11
pavanyalala <pavanyalala4508@gmail.com>
2021-08-05 08:59:01 +0530

    [pavan]:LinkedList Creation added

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        public Node head;
+        /// <summary>
+        /// UC-1
+        /// </summary>
+        /// <param name="data"></param>
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("Inserted values : " + node.data);
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+
+        }
+    }
+}
